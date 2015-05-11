2096b93b7ad2c254f623c35774c35681412e4645
Neal Fultz <njf@zestfinance.com>
2015-05-11 16:57:35 +0000

    version bump

--- R/bsearch7.R
+++ R/bsearch7.R
@@ -2,13 +2,15 @@
 #' 
 #' @param val  values
 #' @param tab  table to find values in
+#' @param L    lower bound
+#' @param H    upper bound
 #' 
 #' @author Martin Morgan, Neal Fultz
 #' @references \url{http://stackoverflow.com/questions/20133344/find-closest-value-in-a-vector-with-binary-search/} and
 #'             \url{https://stat.ethz.ch/pipermail/r-help/2011-April/274182.html}
 #' @export
 #' @examples
-#'  bsearch7(sample(letters, 5000, replace=TRUE, letters))
+#'  bsearch7(sample(letters, 5000, replace=TRUE), letters)
 bsearch7 <-
      function(val, tab, L=1L, H=length(tab))
 {
