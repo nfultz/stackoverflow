301b6c3c5d33c783ead9bf2c89b372b5a7b596f5
Neal Fultz <njf@zestfinance.com>
2015-05-15 19:55:19 +0000

    Adding duplicated2

---
+++ R/duplicated2.R
@@ -0,0 +1,17 @@
+#' Find duplicates in a string
+#' 
+#' This will find all duplicates in a run, unlike \code{\link{duplicated}} which
+#' finds duplicates globally.
+#' 
+#' @param x a vector
+#' 
+#' @export
+#' @author \href{http://stackoverflow.com/users/980833/josh-obrien}{Josh O'Brien}, Neal Fultz 
+#' @references \url{http://stackoverflow.com/questions/30260507/exclude-subsequent-duplicated-rows-in-r}
+#' 
+#' @examples
+#' duplicated2(c(2,3,3,2,2,3,3,3,3,2,2))
+
+duplicated2 <- function(x) {
+  sequence(rle(x)$lengths) > 1
+}
